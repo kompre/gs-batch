3028692fad0e539085d311cb0e24e27adcd06f6c
kompre <s.follador@gmail.com>
2024-09-18 22:18:50 +0200

    progressbar

--- gs_batch/gs_batch.py
+++ gs_batch/gs_batch.py
@@ -7,6 +7,7 @@ import sys
 from click.testing import CliRunner
 from concurrent.futures import ProcessPoolExecutor
 
+
 def human_readable_size(size_in_bytes):
     """Convert file size from bytes to a human-readable format."""
     if size_in_bytes >= 1024 * 1024:
@@ -20,10 +21,30 @@ def human_readable_size(size_in_bytes):
 def run_ghostscript(args):
     """Helper function to run the Ghostscript command."""
     try:
-        subprocess.run(args, check=True)
-        click.echo(f"Ghostscript command executed successfully.")
+        # print(f"Executing Ghostscript: {' '.join(args)}")
+        process = subprocess.Popen(
+            args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        # with click.progressbar(iter(process.stdout.readline, b""), length=56) as line:
+        #     line
+        total_length = None
+        for line in iter(process.stdout.readline, b""):
+            line = line.decode("utf-8")
+            if line.startswith("Processing pages "):
+                total_length = line.split(" ")[-1].replace(".", "")
+                click.echo(f"{total_length=}")
+
+            if total_length:
+                with click.progressbar(
+                    length=int(total_length), color=True, width=10
+                ) as bar:
+                    if line.startswith("Page "):
+                        bar.update(int(line.split(" ")[-1]))
+
+            # click.echo(line)
     except subprocess.CalledProcessError as e:
         click.echo(f"Error executing Ghostscript: {e}")
+
 
 def process_file(file_info):
     """Process a single file with Ghostscript."""
@@ -34,7 +55,7 @@ def process_file(file_info):
         temp_output_file = tmp_output.name
 
     # Base Ghostscript command
-    gs_command = ["gswin64c", "-sDEVICE=pdfwrite", "-q", "-o", temp_output_file]
+    gs_command = ["gs", "-sDEVICE=pdfwrite", "-o", temp_output_file]
     gs_command.extend(command_parts)
     gs_command.append(pdf_file)
 
@@ -43,35 +64,37 @@ def process_file(file_info):
 
     # Rename the output file
     rename_output(temp_output_file, pdf_file, prefix, suffix, keep_smaller)
-    
+
     return result
 
 
 def rename_output(temp_file, original_file, prefix, suffix, keep_smaller):
     """Rename or move the smaller file (old or new) based on the --keep_smaller flag."""
-    
+
     # Get the base name and extension of the original file
     input_basename, input_ext = os.path.splitext(os.path.basename(original_file))
-    
+
     # Form the full output file path with prefix and suffix
     output_file = f"{prefix}{input_basename}{suffix}{input_ext}"
-    
+
     # Ensure the directory exists; if not, create it
     output_dir = os.path.dirname(output_file)
     if output_dir and not os.path.exists(output_dir):
         click.echo(f"Creating directory: {output_dir}")
         os.makedirs(output_dir, exist_ok=True)
-    
+
     # Get sizes of original and temporary files
     original_size = os.path.getsize(original_file)
     new_size = os.path.getsize(temp_file)
-    
+
     column_width = 14
-    
+
     # Print human-readable sizes
-    click.echo(f"{'Original size':^{column_width}s} | {'New size':^{column_width}s} | {'Ratio':^{column_width}s} | {'keeping':^{column_width}s}") 
+    click.echo(
+        f"{'Original size':^{column_width}s} | {'New size':^{column_width}s} | {'Ratio':^{column_width}s} | {'keeping':^{column_width}s}"
+    )
     # Calculate percentage difference
-    ratio = (new_size / original_size) 
+    ratio = new_size / original_size
 
     # Determine which file to keep based on the --keep_smaller flag
     if keep_smaller and new_size >= original_size:
@@ -80,8 +103,10 @@ def rename_output(temp_file, original_file, prefix, suffix, keep_smaller):
     else:
         status = "new"
         shutil.move(temp_file, output_file)
-    click.echo(f"{human_readable_size(original_size):>{column_width}s} | {human_readable_size(new_size):>{column_width}s} | {ratio:{column_width}.3%} | { status:^{column_width}s}")
-    
+    click.echo(
+        f"{human_readable_size(original_size):>{column_width}s} | {human_readable_size(new_size):>{column_width}s} | {ratio:{column_width}.3%} | { status:^{column_width}s}"
+    )
+
 
 @click.command(no_args_is_help=True)
 @click.option(
@@ -108,7 +133,9 @@ def rename_output(temp_file, original_file, prefix, suffix, keep_smaller):
     help="Suffix to add to the output file name before the extension.",
 )
 @click.option(
-    "--keep_smaller/--keep_new", default=True, help="Keep the smaller file between old and new (default: keep smaller)."
+    "--keep_smaller/--keep_new",
+    default=True,
+    help="Keep the smaller file between old and new (default: keep smaller).",
 )
 @click.argument("files", nargs=-1, type=click.Path(exists=True))
 def gs_batch(options, prefix, suffix, compress, pdfa, files, keep_smaller):
@@ -123,10 +150,10 @@ def gs_batch(options, prefix, suffix, compress, pdfa, files, keep_smaller):
         for arg in sys.argv
         if (command := arg.split("=")[0]) in ["--compress", "--pdfa", "--options"]
     ]
-    
+
     ## uncomment for debugging because sys.argv is different using the invoker
     # commands_to_apply = ['--compress']
-    
+
     for command in commands_to_apply:
         match command:
             case "--compress":
@@ -138,31 +165,36 @@ def gs_batch(options, prefix, suffix, compress, pdfa, files, keep_smaller):
             case "--options":
                 command_parts.append(options)
 
-    click.echo(f'Files to process: {len(files)}', color='red')
-    
-     # Prepare file processing tasks
-    file_tasks = [(pdf_file, command_parts, prefix, suffix, keep_smaller) for pdf_file in files]
+    click.echo(f"Files to process: {len(files)}", color="red")
+
+    # Prepare file processing tasks
+    file_tasks = [
+        (pdf_file, command_parts, prefix, suffix, keep_smaller) for pdf_file in files
+    ]
+
+    # for file in file_tasks:
+    #     process_file(file)
 
     # Process files in parallel using ProcessPoolExecutor
     with ProcessPoolExecutor() as executor:
         results = list(executor.map(process_file, file_tasks))
 
-    # Print results
-    for result in results:
-        click.echo(result)
-    
+    # # Print results
+    # for result in results:
+    #     click.echo(result)
+
     # for pdf_file in files:
     #     # Create a temporary output file
     #     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_output:
     #         temp_output_file = tmp_output.name
 
     #     # Base Ghostscript command
-    #     gs_command = ["gswin64c", "-sDEVICE=pdfwrite", "-q", "-o", temp_output_file]
+    #     gs_command = ["gswin64c", "-sDEVICE=pdfwrite", "-o", temp_output_file]
 
     #     # Add the Ghostscript options based on the order captured
     #     for part in command_parts:
     #         gs_command.extend(part.split())
-        
+
     #     # Append the input PDF file
     #     gs_command.append(pdf_file)
 
@@ -176,5 +208,7 @@ def gs_batch(options, prefix, suffix, compress, pdfa, files, keep_smaller):
 
 if __name__ == "__main__":
     runner = CliRunner()
-    result = runner.invoke(gs_batch, r'--compress=/ebook --prefix=".\\compressed\\" mat.pdf')
+    result = runner.invoke(
+        gs_batch, r'--compress=/ebook --prefix=".\\compressed\\" mat.pdf'
+    )
     print(result.output)
---
+++ test_out.py
@@ -0,0 +1,7 @@
+import sys
+import time
+
+print("Hello")
+# sys.stdout.flush()
+time.sleep(3)
+print("World")
